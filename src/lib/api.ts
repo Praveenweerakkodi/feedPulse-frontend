eeab0a58fba3a241844a40777a250b8cd2bb5b85
Praveen Weerakkodi <praveenweerakkodi@gmail.com>
2026-04-05 16:39:36 +0530

    changed api path

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://feed-pulse-backend.vercel.app/api';
 
 export const api = axios.create({
   baseURL: API_URL,
